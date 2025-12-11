2d965d7278f58dc5f6bdbf414753958a54a2741f
sa200022 <k55123k@hotmail.com>
2025-12-11 10:56:48 +0800

    1

--- src/WebhookDelivery.Core/obj/Debug/net8.0/WebhookDelivery.Core.AssemblyInfo.cs
+++ src/WebhookDelivery.Core/obj/Debug/net8.0/WebhookDelivery.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebhookDelivery.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f508140ae167057d9f7e6c39dde8524cfa319e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebhookDelivery.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebhookDelivery.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/WebhookDelivery.Router/obj/Debug/net8.0/WebhookDelivery.Router.AssemblyInfo.cs
+++ src/WebhookDelivery.Router/obj/Debug/net8.0/WebhookDelivery.Router.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebhookDelivery.Router")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f508140ae167057d9f7e6c39dde8524cfa319e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebhookDelivery.Router")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebhookDelivery.Router")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
